498a114f70d51f64d478f2be9f9ff40bd435cce6
Tigris Li <tigris_li@msn.com>
2023-02-05 15:25:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(__file__), 'pic')
-libdir = os.path.join(os.path.dirname(__file__), 'lib')
+picdir = os.path.join(os.getcwd(), 'pic')
+libdir = os.path.join(os.getcwd(), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
