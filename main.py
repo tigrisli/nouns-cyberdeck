98beb770dc729c897c230296def1ed0a6cf4564e
Tigris Li <tigris_li@msn.com>
2023-02-05 21:02:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
