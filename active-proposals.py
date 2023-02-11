0281b323deae101e3f2d392e4bca12a975367c37
Tigris Li <hello@tigris.li>
2023-02-11 01:21:22 +0000

    added if countdown time = 0, don't display prop

--- active-proposals.py
+++ active-proposals.py
@@ -14,12 +14,14 @@ import time
 from datetime import datetime, timedelta
 
 def getCountdownCopy(proposal, currentBlock, activeLocale):
-    endBlock = proposal["endBlock"]
-    startBlock = proposal["startBlock"]
+    endBlock = int(proposal["endBlock"])
+    startBlock = int(proposal["startBlock"])
     blockDuration = endBlock - startBlock
     totalDuration = blockDuration * AVERAGE_BLOCK_TIME_IN_SECS
 
     remainingDuration = totalDuration - (currentBlock - startBlock) * AVERAGE_BLOCK_TIME_IN_SECS
+    if remainingDuration < 0:
+        return "0s"
 
     remainingDays = int(remainingDuration / 86400)
     remainingDuration -= remainingDays * 86400
@@ -33,16 +35,17 @@ def getCountdownCopy(proposal, currentBlock, activeLocale):
     remainingSeconds = int(remainingDuration)
 
     if remainingDays > 0:
-        return str(remainingDays) + "d" + str(remainingHours) + "h" + str(remainingMinutes) + "m"
+        return str(remainingDays) + "d " + str(remainingHours) + "h " + str(remainingMinutes) + "m"
     elif remainingHours > 0:
-        return str(remainingHours) + "h" + str(remainingMinutes) + "m" + str(remainingSeconds) + "s"
+        return str(remainingHours) + "h " + str(remainingMinutes) + "m " + str(remainingSeconds) + "s"
     elif remainingMinutes > 0:
-        return str(remainingMinutes) + "m" + str(remainingSeconds) + "s"
+        return str(remainingMinutes) + "m " + str(remainingSeconds) + "s"
     else:
         return str(remainingSeconds) + "s"
 
 
-# Send the GraphQL query to retrieve the information
+
+# Send GraphQL query to retrieve information
 query = """
 {
   proposals(orderBy: startBlock, orderDirection: desc) {
@@ -69,7 +72,7 @@ else:
     raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, response.text))
 
 
-# Initialize the display
+# Initialize display
 epd = epd2in13b_V3.EPD()
 epd.init()
 
@@ -77,7 +80,7 @@ epd.init()
 background_image = Image.open("Proposal-glasses.bmp")
 background_red_image = Image.open("Proposals-BG-RED.bmp")
 
-# Create an image with the information
+# Create image
 black_image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
 black_image.paste(background_image)
 
@@ -87,7 +90,7 @@ red_image.paste(background_red_image)
 draw_black = ImageDraw.Draw(black_image)
 draw_red = ImageDraw.Draw(red_image)
 
-# Draw the information on the image
+# Draw information on image
 font = ImageFont.truetype('./fonts/LondrinaSolid-Regular.ttf', 16)
 
 title_text = "ACTIVE PROPS"
@@ -106,31 +109,32 @@ for proposal in result["data"]["proposals"]:
         currentBlock = int(result["data"]["proposals"][0]["startBlock"])
         createdTimestamp = int(proposal["createdTimestamp"])
 
-        active_local = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        active_local = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         countdown = getCountdownCopy(proposal, currentBlock, active_local)
+        #if countdown == "0s":
+            #continue  # skip proposal if countdown has reached 0
 
         createdDate = datetime.fromtimestamp(createdTimestamp)
         endTimestamp = createdTimestamp + AVERAGE_BLOCK_TIME_IN_SECS * (endBlock - currentBlock)
         # endDate before voting period starts
         endDate = datetime.fromtimestamp(endTimestamp)
         
-        print(title_text)
-        print(createdDate)
-        print(endDate)
+        print(f"Proposal ID: {id_text}")
+        print(f"Title: {title_text}")
+        print(f"Countdown: {countdown}")
         print(" ")
         
         # Calculate the remaining time
-        remainingTime = (endTimestamp - timestamp)
+        #remainingTime = (endTimestamp - timestamp)
 
-        if remainingTime < 0:
-            remainingTime = 0
+        #if remainingTime < 0:
+        #    remainingTime = 0
 
         # Format the remaining time as a string
-        remainingTime_text = str(timedelta(seconds=remainingTime)).split(".")[0]
+        #remainingTime_text = str(timedelta(seconds=remainingTime)).split(".")[0]
 
-        print(remainingTime_text)
 
-        draw_black.text((5, y+12), remainingTime_text, font=font, fill=0)    
+        draw_black.text((100, y), countdown, font=font, fill=0)    
 
         draw_black.text((5, y), id_text, font=font, fill=0)
 
@@ -152,9 +156,6 @@ for proposal in result["data"]["proposals"]:
         y += 25
 
 
-
-# Display the image on the e-ink display
+# Display image on the e-ink display
 epd.display(epd.getbuffer(black_image), epd.getbuffer(red_image))
-
-# Close the display
 epd.sleep()
