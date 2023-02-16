34800ac3512907c2b061c98ff9a259bd7858011f
Tigris Li <hello@tigris.li>
2023-02-16 10:51:40 +0000

    define fetch noun for vertical

--- fetch-nouns.py
+++ fetch-nouns.py
@@ -1,17 +1,204 @@
-import sys
-import os
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
+import time
 import requests
-import base64
-import cv2
-import numpy as np
-from PIL import Image, ImageDraw
 import traceback
+import re
+import base64
+import json
+import cv2
+import cairosvg
+import numpy as np
+from PIL import Image, ImageDraw, ImageEnhance, ImageFont
 from waveshare_epd import epd2in13b_V3
-from io import BytesIO
+from typing import List, Dict
+
+class NounSeed:
+  def __init__(self, background: int, body: int, accessory: int, head: int, glasses: int):
+    self.background = background
+    self.body = body
+    self.accessory = accessory
+    self.head = head
+    self.glasses = glasses
+
+class EncodedImage:
+  def __init__(self, filename: str, data: str):
+    self.filename = filename
+    self.data = data
+
+class NounData:
+  def __init__(self, parts: List[EncodedImage], background: str):
+    self.parts = parts
+    self.background = background
+
+
+
+def get_noun_data(seed: NounSeed) -> NounData:
+  global palette
+  # load the image data. This is a json file that contains the image data. Make sure you have the image-data.json file in the same directory as this file.
+  images = json.load(open("image-data.json"))
+  bgcolors = images['bgcolors']
+  palette = images['palette']
+  bodies = images['images']['bodies']
+  accessories = images['images']['accessories']
+  heads = images['images']['heads']
+  glasses = images['images']['glasses']
+  return NounData(
+    parts=[
+      bodies[int(seed['body'])],
+      accessories[int(seed['accessory'])],
+      heads[int(seed['head'])],
+      glasses[int(seed['glasses'])]
+    ],
+    background=bgcolors[int(seed['background'])]
+  )
+
+class IEncoder:
+    def encode_image(self, filename: str, image):
+        raise NotImplementedError
+
+
+class Rect:
+    def __init__(self, length, color_index):
+        self.length = length
+        self.color_index = color_index
+
+
+class LineBounds:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class ImageRow:
+    def __init__(self, rects, bounds):
+        self.rects = rects
+        self.bounds = bounds
+
+
+ImageRows = Dict[int, ImageRow]
+
+class ImageBounds:
+    def __init__(self, top, right, bottom, left):
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.left = left
+
+
+class RGBAColor:
+    def __init__(self, r, g, b, a):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+
+
+class DecodedImage:
+    def __init__(self, palette_index, bounds, rects):
+        self.palette_index = palette_index
+        self.bounds = bounds
+        self.rects = rects
+
+
+class EncodedImage:
+    def __init__(self, filename, data):
+        self.filename = filename
+        self.data = data
+
+
+class ImageData:
+    def __init__(self, palette, images):
+        self.palette = palette
+        self.images = images
+
+
+class PngImage:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def rgba_at(self, x, y):
+        raise NotImplementedError
+
+def decode_image(image):
+    data = re.sub(r"^0x", "", image)
+    palette_index = int(data[0:2], 16)
+    bounds = {
+        "top": int(data[2:4], 16),
+        "right": int(data[4:6], 16),
+        "bottom": int(data[6:8], 16),
+        "left": int(data[8:10], 16)
+    }
+    rects = data[10:]
+
+    if rects:
+        rects = [
+            (int(rect[0:2], 16), int(rect[2:4], 16))
+            for rect in re.findall(r".{1,4}", rects)
+        ]
+    else:
+        rects = []
+
+    return {
+        "palette_index": palette_index,
+        "bounds": bounds,
+        "rects": rects
+    }
+
+def get_rect_length(current_x, draw_length, right_bound):
+    remaining_pixels_in_line = right_bound - current_x
+    return draw_length if draw_length <= remaining_pixels_in_line else remaining_pixels_in_line
+
+def build_svg(parts, palette_colors, bg_color=None):
+    svg_without_end_tag = ""
+    for part in parts:
+        svg_rects = []
+        decoded_image = decode_image(part["data"])
+        bounds = decoded_image["bounds"]
+        rects = decoded_image["rects"]
+
+        current_x = bounds["left"]
+        current_y = bounds["top"]
+
+        for draw in rects:
+            draw_length = draw[0]
+            color_index = draw[1]
+            hex_color = palette_colors[color_index]
+
+            length = get_rect_length(current_x, draw_length, bounds["right"])
+            while length > 0:
+                if color_index != 0:
+                    svg_rects.append(
+                        f'<rect width="{length * 10}" height="10" x="{current_x * 10}" y="{current_y * 10}" fill="#{hex_color}" />'
+                    )
+
+                current_x += length
+                if current_x == bounds["right"]:
+                    current_x = bounds["left"]
+                    current_y += 1
+
+                draw_length -= length
+                length = get_rect_length(current_x, draw_length, bounds["right"])
+
+        svg_without_end_tag += "".join(svg_rects)
+
+    return f'<svg width="320" height="320" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges"><rect width="100%" height="100%" fill="{bg_color if bg_color else "none"}" />{svg_without_end_tag}</svg>'
+
+def get_noun(noun_id, seed):
+    id = str(noun_id)
+    name = f"Noun {id}"
+    description = f"Noun {id} is a member of the Nouns DAO"
+    
+    data_ = get_noun_data(seed)
+    parts = data_.parts
+    background = data_.background
+    image = f"data:image/svg+xml;base64,{base64.b64encode(build_svg(parts, palette, background).encode()).decode()}"
+
+    return {
+        "name": name,
+        "description": description,
+        "image": image
+    }
+
 
 # Set the query to fetch the seed data from the Nouns subgraph API
 url = "https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph"
@@ -42,112 +229,98 @@ query {
 }
 '''
 
-def build_svg(noun_data, palette, background):
-    # Create a string that will hold the svg image
-    svg_image = ""
-
-    # Add the opening tag of the svg element
-    svg_image += '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">'
-
-    # Add the background element
-    svg_image += f'<rect width="100" height="100" fill="{palette[background]}"/>'
-
-    # Add the body element
-    svg_image += f'<circle cx="50" cy="50" r="35" fill="{palette[noun_data["body"]]}"/>'
-
-    # Add the accessory element
-    svg_image += f'<rect x="40" y="40" width="20" height="20" fill="{palette[noun_data["accessory"]]}"/>'
-
-    # Add the head element
-    svg_image += f'<circle cx="50" cy="35" r="15" fill="{palette[noun_data["head"]]}"/>'
-
-    # Add the glasses element
-    svg_image += f'<rect x="45" y="30" width="10" height="5" fill="{palette[noun_data["glasses"]]}"/>'
-    svg_image += f'<rect x="45" y="35" width="10" height="5" fill="{palette[noun_data["glasses"]]}"/>'
-
-    # Close the svg tag
-    svg_image += "</svg>"
-
-    return svg_image
-
-
-def get_noun_data(seed):
-    # Retrieve data from noun parts
-    noun_data = {
-      "body": seed["body"],
-      "accessory": seed["accessory"],
-      "head": seed["head"],
-      "glasses": seed["glasses"]
-    }
-    palette = seed["palette"]
-
-    return noun_data, palette
-
-def getNoun(seed):
-    if "id" in seed:
-        id = str(seed["id"])
-        name = "Noun {}".format(id)
-        description = "Noun {} is a member of the Nouns DAO".format(id)
-        parts, background = get_noun_data(seed)
-        image = "data:image/svg+xml;base64,{}".format(base64.b64encode(build_svg(parts, seed["palette"], background).encode()).decode())
-
-        noun = {
-            "name": name,
-            "description": description,
-            "image": image
-        }
-    else:
-        return {}
 
 # Fetch the seed data from the Nouns subgraph API
 try:
     response = requests.post(url, json={'query': query})
     response.raise_for_status()
     result = response.json()
+    # save the result to a file
+    #with open("result.json", "w") as f:
+      #  import json
+     #   json.dump(result, f)
+        
 except Exception as e:
     print(f"Error fetching data from Nouns subgraph API: {e}")
     traceback.print_exc()
 
+
+noun = result["data"]["auctions"][0]["bids"][0]["noun"]["id"]
 # Get the seed data of the first bid in the first auction
 seed = result["data"]["auctions"][0]["bids"][0]["noun"]["seed"]
 
-# Build the SVG image using the `getNoun` function from the previous code example
-noun = getNoun(seed=seed)
+# Get the Noun data
+noun_data = get_noun(noun, seed)
+image_data = noun_data["image"]
+# save image attribute to a file
+with open("noun.svg", "wb") as f:
+    f.write(base64.b64decode(image_data.split(",")[1].encode()))
+    
 
-# Convert the seed data to a base64 encoded SVG string
-if "image" in noun:
-    svg_string = noun["image"].replace("data:image/svg+xml;base64,", "").encode()
-    b64 = base64.b64encode(svg_string).decode()
-    with open("output.jpg", "wb") as f:
-      f.write(base64.b64decode(b64.encode()))
-else:
-    print(f"Error fetching image")
-
-# The final base64 encoded SVG string
-image_data = f"data:image/svg+xml;base64,{b64}"
-
-# Convert base 64 encoded SVG string to an image
-img = Image.open(BytesIO(base64.b64decode(b64)))
-img.show()
-
+# convert the svg image to raster image
+cairosvg.svg2png(url="noun.svg",write_to="noun.png")
 # Load the image data into a NumPy array
-img = np.array(Image.open(BytesIO(base64.b64decode(image_data.split(',')[1].encode()))))
+img = np.array(cv2.imread("noun.png"))
 
 # Resize the image to 122x122
-img = cv2.resize(img, (122, 122), interpolation = cv2.INTER_CUBIC)
+#img = cv2.resize(img, (8, 8), interpolation = cv2.INTER_CUBIC)
 
 # Initialize the display
 epd = epd2in13b_V3.EPD()
 epd.init()
+#epd.Clear()
 
 # Load the resized image
 noun_image = Image.fromarray(img)
+#noun_image = noun_image.transpose(Image.ROTATE_90)
 
-# Create an image with the information
-image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-image.paste(noun_image)
-draw = ImageDraw.Draw(image)
+imageblack_h = Image.new('1',(epd.width ,epd.height),255)
+imageblack_draw = ImageDraw.Draw(imageblack_h)
+imagered_h = Image.new('1',(epd.width ,epd.height),255)
+imagered_draw = ImageDraw.Draw(imagered_h)
+noun_image = noun_image.resize((140,140))
+noun_image = noun_image.convert('RGB')
+enhancer = ImageEnhance.Contrast(noun_image)
+image = enhancer.enhance(2)
+imagered_h.paste(noun_image,(-15,0))
 
-# Display the resized image on the Waveshare 2in13b e-ink display
-epd.display(epd.getbuffer(noun_image))
-epd.sleep()
+epd.display(imageblack_h.tobytes(), imagered_h.tobytes())
+#epd.sleep()
+
+
+
+def get_time_remaining(start_time, end_time):
+    current_time = int(time.time())
+    time_remaining = int(end_time) - current_time
+    minutes, seconds = divmod(time_remaining,60)
+    hours, minutes = divmod(minutes, 60)
+    time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    return time_remaining,time_str
+
+font = ImageFont.truetype('./fonts/LondrinaSolid-Regular.ttf', 16)
+
+noun_number = result["data"]["auctions"][0]["bids"][0]["noun"]["id"]
+current_bid = result["data"]["auctions"][0]["bids"][0]["amount"]
+current_bid = float(current_bid) / 1000000000000000000 # convert to ETH
+start_time = result["data"]["auctions"][0]["startTime"]
+end_time = result["data"]["auctions"][0]["endTime"]
+bid = f"Bid: {current_bid:.2f}ETH"
+imageblack_draw.text((10, 160), "Noun " + str(noun_number), font=font,  fill=0)
+imageblack_draw.text((10, 175), bid, font=font, fill=0)
+
+while True:
+    
+
+    time_remaining,time_str = get_time_remaining(start_time, end_time)
+    if time_remaining <= 5:
+        break
+
+    
+    imageblack_draw.text((10, 190), str(time_str), font=font, fill=0)
+        
+    # Display the information on the e-ink display
+    epd.display(imageblack_h.tobytes(), imagered_h.tobytes())
+        
+    # Wait for 1 second before updating the display again
+    time.sleep(60)
+    
