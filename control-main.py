464017a6bd04823488195614ee59d398d418a0aa
Tigris Li <hello@tigris.li>
2023-02-25 13:15:28 +0000

    button functions

--- control-main.py
+++ control-main.py
@@ -44,7 +44,6 @@ while True:
    time.sleep(.1)
    if button_flag == "button_1":
        buttonshim.set_pixel(0x94, 0x00, 0xd3)
-       os.system('/usr/bin/python /home/tigris/nouns-cyberdeck/active-proposals.py')
        button_flag = "null"
    elif button_flag == "button_2":
        buttonshim.set_pixel(0x00, 0x00, 0xff)
